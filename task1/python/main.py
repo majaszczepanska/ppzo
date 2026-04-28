7cb3de10638c0104d5d985f33f4283a25c6bd3c8
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-04-28 12:13:04 +0200

    add comments

--- task1/python/main.py
+++ task1/python/main.py
@@ -7,6 +7,7 @@ def main():
         print("4. Zakończ program")
         choice = input("Wybierz opcję (1-4): ")
 
+        #Calculator
         if choice == '1':
             print("\nZadanie 1: Prosty kalkulator dwóch liczb.")
             try:
@@ -17,6 +18,7 @@ def main():
                 continue
 
             operation = input("Wybierz operację (+, -, *, /): ")
+            #Prevent division by zero
             if operation == '/' and b == 0:
                 print("Nie można dzielić przez zero.")
                 continue
@@ -34,12 +36,13 @@ def main():
                 continue
             print(f"Wynik: {round(result, 2)}")
 
-
+        #Temperature converter
         elif choice == '2':
             print("\nZadanie 2: Konweter temperatury (Celsjusz <-> Fahrenheit).")
             conversion = input("Wybierz konwersję:"
                               "\n- napisz c jeżeli chcesz zamienić Celsjusza na Fahrenheita"
                               "\n- napisz f jeżeli chcesz zamienić Fahrenheita na Celsjusza): ").lower()
+            #Validate user input (c or f)
             if conversion != "c" and conversion != "f":
                 print("Nieprawidłowy wybór konwersji.")
                 continue
@@ -57,7 +60,7 @@ def main():
                 result = round((temp - 32) / 1.8, 2)
                 print(temp, "°F to", result, "°C")
 
-
+        #Average grade calculator
         elif choice == '3':
             print("\nZadanie 3: Wyliczanie średniej ocen ucznia.")
             try:
@@ -72,13 +75,15 @@ def main():
             sum_of_grades = 0
 
             for i in range(number_of_grades):
+                #Keep asking for grade until user inputs a valid one (between 1 and 6)
                 while True:
                     try:
                         grade = float(input(f"Podaj ocenę: "))
                     except ValueError:
                         print("Nieprawidłowe dane. Musisz podać liczbę.")
                         continue
-                    
+
+                    #Validate grade (1-6)
                     if grade >= 1 and grade <= 6:
                         sum_of_grades += grade
                         break
@@ -87,17 +92,18 @@ def main():
                 
             average = round(sum_of_grades / number_of_grades, 2)
             print(f"Średnia: {average}")
+
             if (average >= 3.0):
                 print("Uczeń zdał.")
             else:
                 print("Uczeń nie zdał.")
 
-
+        #Exit program
         elif choice == '4':
             print("Zakończenie programu.")
             break
 
-
+        #Invalid choice (other than 1-4)
         else:
             print("Nieprawidłowy wybór. Wybierz opcję od 1 do 4.")
 
