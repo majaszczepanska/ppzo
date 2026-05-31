afbf24e417bc81f0293c73ad2f680f8faf04f221
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-05-31 13:10:25 +0200

    update db save

--- task2/csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
+++ task2/csharp/obj/Debug/net8.0/csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75a48c0933b7fa501504c67aca8d2bfbfaa5b08f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cdf8287c267b6a6e020074d2f53a0c461319cf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
