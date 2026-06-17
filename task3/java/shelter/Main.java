a1df99583fd373d63d23ecbf48ed7a8d503b92e1
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-06-17 14:35:02 +0200

    display adoption record and playtime actions

--- task3/java/shelter/Main.java
+++ task3/java/shelter/Main.java
@@ -1,4 +1,4 @@
-package task3.java;
+package task3.java.shelter;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,22 +6,34 @@ public class Main {
         Shelter myShelter = new Shelter();
 
         // Creating objects (Animals and Adopter)
-        Dog dog1 = new Dog("Burek", 3);
-        Dog dog2 = new Dog("Reksio", 1);
-        Cat cat1 = new Cat("Mruczek", 2);
+        Dog dog1 = new Dog("dog1", 3);
+        Dog dog2 = new Dog("dog2", 1);
+        Cat cat1 = new Cat("cat1", 2);
         
-        Adopter adopter1 = new Adopter("Jan Kowalski");
+        Adopter adopter1 = new Adopter("Alice");
 
         // Adding to the shelter
         myShelter.addAnimal(dog1);
         myShelter.addAnimal(dog2);
         myShelter.addAnimal(cat1);
 
-        // Demonstrating polymorphism
         myShelter.showAnimals();
 
-        // Adoption process
-        myShelter.processAdoption(dog1, adopter1);
+        // Adoption process - first version (direct method call)
+        //myShelter.processAdoption(dog1, adopter1);
+
+        //Demonstrating polymorphism and interface usage
+        System.out.println("--- Playtime ---");
+        dog1.play();
+        cat1.play();
+        System.out.println("----------------\n");
+
+        // Using AdoptionRecord to process adoption
+        if (myShelter.hasAnimal(dog1)) {
+            AdoptionRecord record = new AdoptionRecord(dog1, adopter1);
+            record.process();
+            myShelter.removeAnimal(dog1); // Removing the adopted animal from the shelter
+        }
 
         // Checking the state after adoption
         myShelter.showAnimals();
